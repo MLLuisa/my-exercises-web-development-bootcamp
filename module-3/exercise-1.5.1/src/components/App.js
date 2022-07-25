f97414da4c3641b9a66faabc4b340784f0cd5844
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-07-25 16:46:25 +0200

    Add exercise 1.5.1 module 3

---
+++ module-3/exercise-1.5.1/src/components/App.js
@@ -0,0 +1,17 @@
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <body>
+    <main className="main">
+      <h1 className="titulo">#CC212A Fire Engine Red</h1>
+    </main>
+    <footer className="footer">
+      <p className="bolita">R</p>
+      <small>&copy 2022 FER</small>
+    </footer>
+    </body>
+  );
+}
+
+export default App;
