9845bdbcb9ccb132910ac89787823accece7c332
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-07-25 16:01:40 +0200

    Add exercise 1 module 3 lesson 1.5

---
+++ module-3/exercise-1-1.5/src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1 className="title">Hola mundo</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
