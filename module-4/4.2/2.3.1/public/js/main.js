377b1a0546211709ab5a9855efb1d8a93bab62e9
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-09-04 15:50:16 +0200

    add exercise 2.3.1 4.2 module 4

---
+++ module-4/4.2/2.3.1/public/js/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// post new user
+
+document.querySelector('.js-btn-post-new-user').addEventListener('click', () => {
+  const inputName = document.querySelector('.js-input-name');
+  const inputEmail = document.querySelector('.js-input-email');
+  const FilterByName = document.querySelector('js-input-newName');
+
+  // create query params
+  const queryParams = `?userName=${inputName.value}&userEmail=${inputEmail.value}&newName=${FilterByName}`;
+
+  fetch('http://localhost:3000/user' + queryParams, { method: 'POST' })
+    .then(response => response.json())
+    .then(responseData => {
+      console.log('Server response:', responseData);
+      printJson('.js-post-new-user-result', responseData);
+    });
+});
+
+// get users data
+
+document.querySelector('.js-btn-get-users').addEventListener('click', () => {
+  fetch('http://localhost:3000/users')
+    .then(response => response.json())
+    .then(responseData => {
+      console.log('Server response:', responseData);
+      printJson('.js-get-users-result', responseData);
+    });
+});
+
+// helper
+
+const printJson = (selector, jsonData) => {
+  const jsonHtml = JSON.stringify(jsonData, null, 2);
+  document.querySelector(selector).innerHTML = jsonHtml;
+};
